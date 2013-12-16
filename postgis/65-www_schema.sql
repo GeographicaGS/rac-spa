07443b691d2fde85a0c3344c16ed112e0f454969
Juan Pedro Perez <jp.alcantara@gstgis.com>
2013-12-16 11:58:01 +0100

    Added FR language to bottom and communities, created the www schema and the survey master view.

---
+++ postgis/65-www_schema.sql
@@ -0,0 +1,53 @@
+-- Survey points
+
+create or replace view www.survey as
+with species as(
+  select
+    a.id_survey as id_survey,
+    array_agg(c.name) as species
+  from
+    data.survey a inner join
+    data.survey_species b on
+    a.id_survey=b.id_survey inner join
+    data.species c on
+    b.id_species=c.id_species
+  where
+    original and geom is not null
+  group by
+    a.id_survey
+),
+communities as(
+  select
+    a.id_survey as id_survey,
+    array_agg(c.description_fr) as community
+  from
+    data.survey a inner join
+    data.survey_community b on
+    a.id_survey=b.id_survey inner join
+    data.community c on
+    b.id_community=c.id_community
+  where
+    original and geom is not null
+  group by
+    a.id_survey
+)
+select
+  a.id_survey as id_survey,
+  a.name as name,
+  -depth as depth,
+  b.description_fr as bottom,
+  community,
+  species,
+  geom
+from
+  data.survey a inner join
+  data.bottom b on
+  a.id_bottom=b.id_bottom inner join
+  species c on
+  a.id_survey=c.id_survey inner join
+  communities d on
+  a.id_survey=d.id_survey
+where
+  original and geom is not null
+order by
+  a.id_survey;
